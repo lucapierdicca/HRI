2f442b2a73046d2406b18689e4aa76de04266afa
lucapierdicca <luca.pierdicca@gmail.com>
2019-07-15 16:56:18 +0200

    render test

--- pnp_modim.py
+++ pnp_modim.py
@@ -6,8 +6,7 @@ sys.path.insert(0, pdir+'/PNPnaoqi/py')
 import pnp_cmd_naoqi
 from pnp_cmd_naoqi import *
 
-from pprint import pprint 
-
+import render
 
 
 if __name__ == "__main__":
@@ -17,14 +16,23 @@ if __name__ == "__main__":
 
 	p.begin()
 
+	# 1. presentazione
+
+
+	# 2. presentazione goal esercizio
+	render.goal_p()
 
 	p.exec_action('Show', '')
-
-	p.exec_action('Say', 'Hi')
-
-	
+	p.exec_action('Say', 'Hi_can_you_help_me?')
 
 
+	# 3. svolgimento esercizio guidato
+
+	# 4. esecuzione plan_tessere
+
+	# 5. gioco
+
+	# 6. saluto
 
 
 	p.end()
--- render.py
+++ render.py
@@ -1,12 +1,29 @@
-import qi
 import time
 import cv2
 import sys
+from random import *
 
 # genera randomicamente l'immagine del goal_p
 def goal_p():
-	bodyparts_to_coord = {'os':(304,65),
-						  'od':(362,429)}
+	bodyparts_to_coord = {'ors':(241,74),
+						  'ord':(113,73),
+						  'ocs':(153,84),
+						  'ocd':(198,84)}
+
+	bodyparts_sample = sample(bodyparts_to_coord.keys(),2)
+	img = cv2.imread('./imgs/goal_p_half.png',1)
+
+	for index,bp in enumerate(bodyparts_sample):
+		cv2.circle(img,bodyparts_to_coord[bp],10,(255,0,0),-1)
+		cv2.putText(img,str(index+1), 
+			(bodyparts_to_coord[bp][0]-6,bodyparts_to_coord[bp][1]+5), 
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2)
+
+	cv2.imwrite('./imgs/img.png',img)
+
+
+
+
 
 '''
 session = qi.Session()
